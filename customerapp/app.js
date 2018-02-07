f6b2a5096eb12806dee1ceb5b5f8a85d4f67504f
Javier Elizalde <javelisol@gmail.com>
2018-02-07 01:50:18 -0600

    created files and put first details, everything is working so far

---
+++ customerapp/app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+/*
+var logger = function(req, res, next){
+    console.log('Logging...');
+    next();
+}
+
+app.use(logger);
+*/
+
+// View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+// Set Static path
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Costumers'
+  });
+});
+
+app.listen(3000, function(){
+  console.log('Server Started on Port 3000...');
+})
