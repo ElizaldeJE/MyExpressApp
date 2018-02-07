1ed40317ef4e4eea77a33c72f6252f1d1d0f1c8d
Javier Elizalde <javelisol@gmail.com>
2018-02-07 02:57:30 -0600

    list is showing in the local server and also the boxes for first/last name & email

--- customerapp/app.js
+++ customerapp/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var expressValidator = require('express-validator');
 
 var app = express();
 
@@ -24,10 +25,41 @@ app.use(bodyParser.urlencoded({extended:false}));
 // Set Static path
 app.use(express.static(path.join(__dirname, 'public')));
 
+var users =[
+  {
+    id: 1,
+    first_name: 'John',
+    last_name: 'Doe',
+    email: 'johndoe@gmail.com',
+  },
+  {
+    id: 2,
+    first_name: 'Bob',
+    last_name: 'Smith',
+    email: 'bobsmith@gmail.com',
+  },
+  {
+    id: 3,
+    first_name: 'Jill',
+    last_name: 'Jackson',
+    email: 'jjackson@gmail.com',
+  }
+];
+
 app.get('/', function(req, res){
   res.render('index', {
-    title: 'Costumers'
+    title: 'Costumers',
+    users: users
   });
+});
+
+app.post('/users/add', function(req, res){
+    var  newUser = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+    }
+    console.log(newUser);
 });
 
 app.listen(3000, function(){
